0fa8a5a69726c100d4ed9a35e34dbe53fc8e52ce
albertoag36 <albertoag36@educastur.es>
2026-09-16 12:25:26 +0200

    Clase 16 09 pt3

--- UT_01/EJER_01/1.5.js
+++ UT_01/EJER_01/1.5.js
@@ -1,41 +1,56 @@
 const estudiantes = [estudiante1 = {
     nombre: "Alberto",
     apellidos: "Alves Garcia",
+    calificacion: 8,
     aprobado: true
 }, estudiante2 = {
     nombre: "Pepe",
     apellidos: "Garcia Garcia",
+    calificacion: 0,
     aprobado: true
 }, estudiante3 = {
     nombre: "Manolo",
     apellidos: "Alves Alves",
+    calificacion: 5,
     aprobado: false
 }]
 
-console.log(estudiantes)
+// console.log(estudiantes)
 
-//Utiliza el método .map() para crear un nuevo array donde a cada 
-// estudiante se le añade una propiedad ‘id’ única (puede ser 1, 2, 3…).
 estudiantesID = []
 
-estudiantes.map(function(id) {
-    for (let estudiantesN of estudiantes){
-        estudiantes.id(estudiantes.findIndex(estudiantesN))
-    }
+let id = 1
 
-    return  estudiantesID.push(estudiantes)
+estudiantes.map(function (estudiante) {
+    estudiante.id = id++
+
+    return estudiantesID.push(estudiante)
 })
 
-//Utiliza el método .filter() para obtener un array que contenga únicamente 
-// a los estudiantes que tienen una calificación mayor o igual a 5.
+// console.log(estudiantesID)
 
-// Para cada estudiante en el array filtrado (los aprobados), imprime un mensaje 
+estudiantesFiltrados = []
+
+estudiantes.map(function (estudiante) {
+    if (estudiante.calificacion >= 5)  return estudiantesFiltrados.push(estudiante)
+})
+
+console.log(estudiantesFiltrados)
+
+// Para cada estudiante en el array filtrado (los aprobados), imprime un mensaje
 // en consola usando Template Strings que diga: “¡Felicidades [nombre], has aprobado con [calificacion]!”.
 
+estudiantes.map(function (estudiante) {
+    if (estudiante.aprobado)  return estudiantesFiltrados.push(estudiante)
+})
 
-// Recorre el array original y verifica si el valor de la propiedad aprobado es 
-// coherente con la calificacion (es decir, si calificacion >= 5 entonces aprobado 
+estudiantesFiltrados.forEach(estudiante => {
+    console.log(`¡Felicidades ${estudiante.nombre}, has aprobado con ${estudiante.calificacion}`)
+});
+
+// Recorre el array original y verifica si el valor de la propiedad aprobado es
+// coherente con la calificacion (es decir, si calificacion >= 5 entonces aprobado
 // debe ser true, y si es menor, debe ser false).
 // Si hay incoherencias, imprime un mensaje como:
-// “⚠️ Incoherencia en el registro de [nombre]: calificación = [calificacion], 
+// “⚠️ Incoherencia en el registro de [nombre]: calificación = [calificacion],
 // aprobado = [aprobado]”
